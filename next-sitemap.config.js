ac8d139c8d3290c1ecc38f2a50470fc05ac59b4d
Olena.Holub <oleholu@gmail.com>
2025-08-21 13:20:47 +0200

    Update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,6 @@ module.exports = {
     siteUrl: 'https://olena-holub-personal-site.vercel.app',
     generateRobotsTxt: true,
     generateIndexSitemap: false,
-    sitemapSize: 50000,
+    sitemapSize: 1000,
   };
   
